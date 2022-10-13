0078eb43b5a38cc0adece3bae77e9810f41455c7
AdiJoost <adi.joost@gmail.com>
2022-10-13 11:43:44 +0200

    data rewritten

--- code.py
+++ code.py
@@ -1,11 +1,26 @@
 import matplotlib.pyplot as plt
 import csv
+import pandas as pd
 
 def main():
-    _, activities = get_csv("csv_data/activity.csv")
-    _, sleeps = get_csv("csv_data/sleep.csv")
-    print(activities)
-    print(sleeps)
+    activities_header, activities = get_csv("csv_data/activity.csv")
+    sleeps_header, sleeps = get_csv("csv_data/sleep.csv")
+    activity = pd.read_csv("csv_data/cool_activity.csv")
+    steps = activity[["Datum", "Schritte"]]
+
+    steps["Schritte"] = pd.to_numeric(steps["Schritte"])
+    print(steps)
+    
+
+
+
+
+
+def print_csv(header: list, data: list):
+    print(header)
+    for row in data:
+        print (f"{row}\n_______________")
+
 
 def get_csv(file_name: str) -> list:
     with open(file_name, "r") as file:
