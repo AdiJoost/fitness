47582fd38e5e3f080d1bab95e136a76a5a81e48e
AdiJoost <adi.joost@gmail.com>
2022-10-13 15:02:09 +0200

    plot on one

--- code.py
+++ code.py
@@ -6,9 +6,23 @@ def main():
     activity = pd.read_csv("csv_data/cool_activity.csv")
     steps = activity[["Datum", "Schritte"]]
 
-    steps["Schritte"] = pd.to_numeric(steps["Schritte"])
-    steps["Datum"] = pd.to_datetime(steps["Datum"], dayfirst=True)
-    steps.plot(x="Datum")
+    steps.loc["Schritte"] = pd.to_numeric(steps["Schritte"])
+    steps.loc["Datum"] = pd.to_datetime(steps["Datum"], dayfirst=True)
+
+    sleeps = pd.read_csv("csv_data/cool_sleeps_singleton.csv")
+    sleeps["Datum"] = pd.to_datetime(sleeps["Startzeit"], dayfirst=True).dt.date
+    sleep_time = sleeps[["Datum", "Minuten geschlafen"]]
+    sleep_time.loc["Minuten geschlafen"] = pd.to_numeric(sleep_time["Minuten geschlafen"])
+    sleep_time["Minuten geschlafen"] = sleep_time["Minuten geschlafen"] * 50
+    
+
+    my_data = pd.concat([steps, sleep_time], axis=1)
+    
+
+    #ax = steps.plot(x="Datum")
+    #sleep_time.plot(x="Datum")
+    my_data.plot()
+    print(my_data)
     plt.show()
     
 
--- csv_replacer.py
+++ csv_replacer.py
@@ -1,3 +1,4 @@
+
 with open ("csv_data/sleep.csv", "r") as file:
     myData = file.read()
 with open("csv_data/cool_sleeps.csv", "w") as file:
